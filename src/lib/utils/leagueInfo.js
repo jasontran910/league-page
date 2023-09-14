00a670977a5bbd79b83fe7cc67eeaa9a28086a64
jasontran910 <55199001+jasontran910@users.noreply.github.com>
2023-09-14 10:11:10 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,7 +8,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p></p>
-  <img src="src/Images/photo_2022-12-28_14-13-36.jpg">
+  <img src="https://imgur.com/a/hUTgcdi">
 `;
 
 /*   STEP 3   */
